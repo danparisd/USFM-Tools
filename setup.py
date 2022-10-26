2c1e5021bc543589ba84da3c0a4fb2f4016f724c
linearcombination <4829djaskdfj@gmail.com>
2022-10-26 14:35:08 -0700

    Strictly typed for mypy and mypyc
    Remove all the non-HTML cases/code.

--- setup.py
+++ setup.py
@@ -8,14 +8,10 @@ setup(
     description="A framework for transforming .usfm files into specified targets",
     license="MIT",
     keywords="unfoldingWord usfm tools",
-    url="https://github.com/unfoldingWord-dev/USFM-Tools",
-    packages=['usfm_tools', 'usfm_tools/support'],
-    long_description='This project comprises a framework for transforming .usfm files into specified targets. It is '
-                     'primarily used for the Open English Bible, and may need adjustment if used for other purposes. '
-                     'This fork of USFM-Tools includes basic support for conversion to USX.',
+    url="https://github.com/linearcombination/USFM-Tools",
+    packages=["usfm_tools", "usfm_tools/support"],
+    long_description="This project comprises a framework for transforming .usfm files into specified targets. It is "
+    "primarily used for the Open English Bible, and may need adjustment if used for other purposes. ",
     classifiers=[],
-    install_requires=[
-        'future==0.18.2',
-        'pyparsing==2.4.5'
-    ]
+    install_requires=["future", "pyparsing"],
 )
